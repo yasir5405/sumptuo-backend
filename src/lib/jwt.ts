415b8ac169c49219eac9bdad7bc4e5260c8e481b
yasir5405 <yasirnaseem5415@gmail.com>
2026-04-01 17:57:23 +0530

    reset password wired into fe

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -4,7 +4,7 @@ const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!;
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
 
 export const generateToken = (userId: number) => {
-  const accessToken = jwt.sign({ userId }, ACCESS_SECRET, { expiresIn: "1d" });
+  const accessToken = jwt.sign({ userId }, ACCESS_SECRET, { expiresIn: "1m" });
   const refreshToken = jwt.sign({ userId }, REFRESH_SECRET, {
     expiresIn: "7d",
   });
