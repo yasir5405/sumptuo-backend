30702ee2e378d1e887673cace9c231f792bbe7a6
yasir5405 <yasirnaseem5415@gmail.com>
2026-04-01 20:29:47 +0530

    Add getAlltransactions controller

--- src/modules/transactions/transaction.controller.ts
+++ src/modules/transactions/transaction.controller.ts
@@ -1,14 +1,10 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { addTransactionSchema } from "./transaction.validation";
 import { ApiResponse } from "../../schema/general.schema";
 import { prisma } from "../../lib/prisma";
-import { Prisma } from "../../generated/prisma/client";
+import { Prisma, Transaction } from "../../generated/prisma/client";
 
-export const addTransaction = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const addTransaction = async (req: Request, res: Response) => {
   const parsedBody = addTransactionSchema.safeParse(req.body);
 
   if (!parsedBody.success) {
@@ -54,24 +50,81 @@ export const addTransaction = async (
       },
     });
 
-    if (!transaction) {
-      const response: ApiResponse<null> = {
-        data: null,
-        message: "Error adding transaction",
-        success: false,
-        error: {
-          message: "Error adding transaction",
-        },
-      };
-      return res.status(500).json(response);
-    }
-
     const response: ApiResponse<null> = {
       data: null,
       message: "Transaction created successfully",
       success: true,
     };
     res.status(201).json(response);
+  } catch (error) {
+    const response: ApiResponse<null> = {
+      data: null,
+      message: "Something went wrong",
+      success: false,
+      error: {
+        message: "Internal server error",
+      },
+    };
+    return res.status(500).json(response);
+  }
+};
+
+export const getAllTransactions = async (req: Request, res: Response) => {
+  const page = Math.max(1, Number(req.query.page) || 1);
+  const limit = Math.min(100, Math.max(1, Number(req.query.limit) || 20));
+  const skip = (page - 1) * limit;
+
+  try {
+    const user = req.user!;
+
+    const [transactions, count] = await prisma.$transaction([
+      prisma.transaction.findMany({
+        where: {
+          userId: user.id,
+        },
+        skip,
+        take: Number(limit),
+        orderBy: {
+          createdAt: "desc",
+        },
+        select: {
+          amount: true,
+          category: true,
+          convertedAmount: true,
+          currency: true,
+          date: true,
+          description: true,
+          importId: true,
+          type: true,
+          id: true,
+          createdAt: true,
+        },
+      }),
+
+      prisma.transaction.count({ where: { userId: user.id } }),
+    ]);
+
+    const totalPages = Math.ceil(count / limit);
+
+    const response: ApiResponse<{
+      transactions: typeof transactions;
+      count: number;
+      page: number;
+      limit: number;
+      totalPages: number;
+    }> = {
+      data: {
+        transactions,
+        count,
+        limit,
+        page,
+        totalPages,
+      },
+      message: "All transactions fetched",
+      success: true,
+    };
+
+    res.status(200).json(response);
   } catch (error) {
     const response: ApiResponse<null> = {
       data: null,
--- src/modules/transactions/transaction.route.ts
+++ src/modules/transactions/transaction.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addTransaction } from "./transaction.controller";
+import { addTransaction, getAllTransactions } from "./transaction.controller";
 import { verifyUser } from "../../middleware/auth.middleware";
 
 const transactionRouter = Router();
 
 transactionRouter.post("/", verifyUser, addTransaction);
+transactionRouter.get("/", verifyUser, getAllTransactions);
 
 export { transactionRouter };
